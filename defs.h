d56a0b2f84422d34d1ce6664aa4e02d466e4dce4
Vatsal Unadkat <vatoobhai@gmail.com>
2017-10-20 16:31:10 -0700

    commit 2 + changes

---
+++ defs.h
@@ -0,0 +1,48 @@
+#ifndef DEFS_H
+#define DEFS_H
+
+typedef unsigned long long U64;
+
+#define NAME "Vice 1.0"
+#define BRD_SQ_NUM 120
+
+enum { EMPTY, wP, wN, wB, wR, wQ, wK, bP, bN, bB, bR, bQ, bK };
+enum { FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H, FILE_NONE };
+enum { RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8, RANK_NONE };
+enum { WHITE, BLACK, BOTH };
+enum {
+	A1 = 21, B1, C1, D1, E1, F1, G1, H1,
+	A2 = 31, B2, C2, D2, E2, F2, G2, H2,
+	A3 = 41, B3, C3, D3, E3, F3, G3, H3,
+	A4 = 41, B4, C4, D4, E4, F4, G4, H4,
+	A5 = 41, B5, C5, D5, E5, F5, G5, H5,
+	A6 = 41, B6, C6, D6, E6, F6, G6, H6,
+	A7 = 41, B7, C7, D7, E7, F7, G7, H7,
+	A8 = 41, B8, C8, D8, E8, F8, G8, H8,
+};
+
+enum { FALSE, TRUE };
+
+typedef struct {
+
+	int pieces[BRD_SQ_NUM];
+	U64 pawns[3]; /*white 1 black 1 both 2*/	
+	
+	int KingSq[2];
+
+	int side; /*current side to move*/
+	int enPas;
+	int fiftyMove; /*draw*/
+	int ply;
+	int hisPly;
+
+	U64 posKey; /*key for each position*/
+
+	int pceNum[13]; /*piece number*/
+	int bigPce[3]; /*non pawns*/
+	int majPce[3]; /*rooks and queens*/
+	int minPce[3]; /*bishops and knights*/
+	
+} S_BOARD;
+
+#endif
