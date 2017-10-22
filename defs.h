56f1d0fd7889d42a9ae665172eacc269eeb5e3ef
Vatsal Unadkat <vatoobhai@gmail.com>
2017-10-22 16:32:20 -0700

    commit 6 hashkeys + random

--- defs.h
+++ defs.h
@@ -103,6 +103,9 @@ extern int Sq120ToSq64[BRD_SQ_NUM];
 extern int Sq64ToSq120[64];
 extern U64 SetMask[64];
 extern U64 ClearMask[64];
+extern U64 PieceKeys[13][120];
+extern U64 SideKey;
+extern U64 CastleKeys[16];
 
 /* FUNCTIONS */
 
@@ -113,5 +116,10 @@ extern void AllInit();
 extern void PrintBitBoard(U64 bb);
 extern int PopBit(U64 *bb);
 extern int CountBits(U64 b);
+
+// hashkeys.c
+extern U64 GeneratePosKey(const S_BOARD *pos);
+
+
 
 #endif
--- init.c
+++ init.c
@@ -1,12 +1,40 @@
-// init.c
-
 #include "defs.h"
+#include "stdlib.h"
+
+/* filling 64 bits with random numbers */
+#define RAND_64 ( (U64)rand() + \
+		  (U64)rand() << 15 + \
+		  (U64)rand() << 30 + \
+		  (U64)rand() << 45 + \
+		  ((U64)rand() & 0xf) << 60 )
+					
 
 int Sq120ToSq64[BRD_SQ_NUM];
 int Sq64ToSq120[64];
 
 U64 SetMask[64];
 U64 ClearMask[64];
+
+U64 PieceKeys[13][120];
+U64 SideKey;
+U64 CastleKeys[16];
+
+/* filling variables with random numbers */
+void InitHashKeys() {
+	
+	int index = 0;
+	int index2 = 0;
+	for(index = 0; index < 13; ++index) {
+		for(index2 = 0; index2 < 120; ++index2) {
+			PieceKeys[index][index2] = RAND_64;
+		}
+	}
+	SideKey = RAND_64;
+	for(index = 0; index < 16; ++index) {
+		CastleKeys[index] = RAND_64;
+	}
+
+}
 
 void InitBitMasks() {
 	int index = 0;
@@ -50,4 +78,5 @@ void InitSq120To64() {
 void AllInit() {
 	InitSq120To64();	
 	InitBitMasks();
+	InitHashKeys();
 }
