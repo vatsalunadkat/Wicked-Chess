3ad7d3151697ff27005120e9eef0dd4e6e3281d6
Vatsal Unadkat <vatoobhai@gmail.com>
2017-10-22 20:49:39 -0700

    commit 10 dealing with castling

--- movegen.c
+++ movegen.c
@@ -161,6 +161,23 @@ void GenerateAllMoves(const S_BOARD *pos, S_MOVELIST *list) {
 				AddCaptureMove(pos, MOVE(sq,sq + 11,EMPTY,EMPTY,MFLAGEP), list);
 			}
 		}
+		
+		if(pos->castlePerm & WKCA) {
+			if(pos->pieces[F1] == EMPTY && pos->pieces[G1] == EMPTY) {
+				if(!SqAttacked(E1,BLACK,pos) && !SqAttacked(F1,BLACK,pos) ) {
+					printf("WKCA MoveGen\n");
+				}
+			}
+		}
+		
+		if(pos->castlePerm & WQCA) {
+			if(pos->pieces[D1] == EMPTY && pos->pieces[C1] == EMPTY && pos->pieces[B1] == EMPTY) {
+				if(!SqAttacked(E1,BLACK,pos) && !SqAttacked(D1,BLACK,pos) ) {
+					printf("WQCA MoveGen\n");
+				}
+			}
+		}
+		
 	} else {
 		
 		for(pceNum = 0; pceNum < pos->pceNum[bP]; ++pceNum) {
@@ -189,6 +206,23 @@ void GenerateAllMoves(const S_BOARD *pos, S_MOVELIST *list) {
 				AddCaptureMove(pos, MOVE(sq,sq - 11,EMPTY,EMPTY,MFLAGEP), list);
 			}
 		}		
+		
+		// castling
+		if(pos->castlePerm &  BKCA) {
+			if(pos->pieces[F8] == EMPTY && pos->pieces[G8] == EMPTY) {
+				if(!SqAttacked(E8,WHITE,pos) && !SqAttacked(F8,WHITE,pos) ) {
+					printf("BKCA MoveGen\n");
+				}
+			}
+		}
+		
+		if(pos->castlePerm &  BQCA) {
+			if(pos->pieces[D8] == EMPTY && pos->pieces[C8] == EMPTY && pos->pieces[B8] == EMPTY) {
+				if(!SqAttacked(E8,WHITE,pos) && !SqAttacked(D8,WHITE,pos) ) {
+					printf("BQCA MoveGen\n");
+				}
+			}
+		}
 	}
 	
 	/* Loop for slide pieces */
@@ -257,7 +291,7 @@ void GenerateAllMoves(const S_BOARD *pos, S_MOVELIST *list) {
 		}
 				
 		pce = LoopNonSlidePce[pceIndex++];
-	}	
+	}
 }
 
 
