1f0934fd89aec92e31d8a097e5750fafb82021b4
Vatsal Unadkat <vatoobhai@gmail.com>
2017-10-28 10:42:04 +0530

    commit 11 adding takemove

--- defs.h
+++ defs.h
@@ -159,6 +159,7 @@ extern int PieceMaj[13];
 extern int PieceMin[13];
 extern int PieceVal[13];
 extern int PieceCol[13];
+extern int PiecePawn[13];
 
 extern int FilesBrd[BRD_SQ_NUM];
 extern int RanksBrd[BRD_SQ_NUM];
