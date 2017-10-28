7d465bf07da699b575a377c200d583556be9c7cf
Vatsal Unadkat <vatoobhai@gmail.com>
2017-10-28 09:36:32 +0530

    commit 11

--- makemove.c
+++ makemove.c
@@ -1,5 +1,8 @@
 // makemove.c
 
+#include "defs.h"
+#include "stdio.h"
+
 #define HASH_PCE(pce,sq) (pos->posKey ^= (PieceKeys[(pce)][(sq)]))
 #define HASH_CA (pos->posKey ^= (CastleKeys[(pos->castlePerm)]))
 #define HASH_SIDE (pos->posKey ^= (SideKey))
@@ -19,3 +22,133 @@ const int CastlePerm[120] = {
     15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
     15, 15, 15, 15, 15, 15, 15, 15, 15, 15
 };
+
+static void ClearPiece(const int sq, S_BOARD *pos) {
+
+	ASSERT(SqOnBoard(sq));
+	
+    int pce = pos->pieces[sq];
+	
+    ASSERT(PieceValid(pce));
+	
+	int col = PieceCol[pce];
+	int index = 0;
+	int t_pceNum = -1;
+	
+    HASH_PCE(pce,sq);
+	
+	pos->pieces[sq] = EMPTY;
+    pos->material[col] -= PieceVal[pce];
+	
+	if(PieceBig[pce]) {
+			pos->bigPce[col]--;
+		if(PieceMaj[pce]) {
+			pos->majPce[col]--;
+		} else {
+			pos->minPce[col]--;
+		}
+	} else {
+		CLRBIT(pos->pawns[col],SQ64(sq));
+		CLRBIT(pos->pawns[BOTH],SQ64(sq));
+	}
+	
+	for(index = 0; index < pos->pceNum[pce]; ++index) {
+		if(pos->pList[pce][index] == sq) {
+			t_pceNum = index;
+			break;
+		}
+	}
+	
+	ASSERT(t_pceNum != -1);
+	
+	pos->pceNum[pce]--;		
+	
+	pos->pList[pce][t_pceNum] = pos->pList[pce][pos->pceNum[pce]];
+  
+}
+
+
+static void AddPiece(const int sq, S_BOARD *pos, const int pce) {
+
+    ASSERT(PieceValid(pce));
+    ASSERT(SqOnBoard(sq));
+	
+	int col = PieceCol[pce];
+
+    HASH_PCE(pce,sq);
+	
+	pos->pieces[sq] = pce;
+
+    if(PieceBig[pce]) {
+			pos->bigPce[col]++;
+		if(PieceMaj[pce]) {
+			pos->majPce[col]++;
+		} else {
+			pos->minPce[col]++;
+		}
+	} else {
+		SETBIT(pos->pawns[col],SQ64(sq));
+		SETBIT(pos->pawns[BOTH],SQ64(sq));
+	}
+	
+	pos->material[col] += PieceVal[pce];
+	pos->pList[pce][pos->pceNum[pce]++] = sq;
+	
+}
+
+static void MovePiece(const int from, const int to, S_BOARD *pos) {
+
+    ASSERT(SqOnBoard(from));
+    ASSERT(SqOnBoard(to));
+	
+	int index = 0;
+	int pce = pos->pieces[from];	
+	int col = PieceCol[pce];
+	
+#ifdef DEBUG
+	int t_PieceNum = FALSE;
+#endif
+
+	HASH_PCE(pce,from);
+	pos->pieces[from] = EMPTY;
+	
+	HASH_PCE(pce,to);
+	pos->pieces[to] = pce;
+	
+	if(!PieceBig[pce]) {
+		CLRBIT(pos->pawns[col],SQ64(from));
+		CLRBIT(pos->pawns[BOTH],SQ64(from));
+		SETBIT(pos->pawns[col],SQ64(to));
+		SETBIT(pos->pawns[BOTH],SQ64(to));		
+	}    
+	
+	for(index = 0; index < pos->pceNum[pce]; ++index) {
+		if(pos->pList[pce][index] == from) {
+			pos->pList[pce][index] = to;
+#ifdef DEBUG
+			t_PieceNum = TRUE;
+#endif
+			break;
+		}
+	}
+	ASSERT(t_PieceNum);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
