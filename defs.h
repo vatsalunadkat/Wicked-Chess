911153e5fab63ff05636463935809c0cb44e570b
Vatsal Unadkat <vatoobhai@gmail.com>
2017-10-29 17:52:59 +0530

    Commit 12 - Passing moves

--- defs.h
+++ defs.h
@@ -124,6 +124,8 @@ typedef struct {
 #define MFLAGCAP 0x7C000
 #define MFLAGPROM 0xF00000
 
+#define NOMOVE 0
+
 
 /* MACROS */
 
@@ -197,6 +199,7 @@ extern int SqAttacked(const int sq, const int side, const S_BOARD *pos);
 extern char *PrMove(const int move);
 extern char *PrSq(const int sq);
 extern void PrintMoveList(const S_MOVELIST *list);
+extern int ParseMove(char *ptrChar, S_BOARD *pos);
 
 //validate.c
 extern int SqOnBoard(const int sq);
@@ -214,6 +217,10 @@ extern void TakeMove(S_BOARD *pos);
 
 // perft.c 
 extern void PerftTest(int depth, S_BOARD *pos);
+
+// search.c
+extern void SearchPosition(S_BOARD *pos);
+
 #endif
 
 
--- makefile
+++ makefile
@@ -1,2 +1,2 @@
 all:
-	gcc vice.c init.c bitboards.c hashkeys.c board.c data.c attack.c io.c movegen.c validate.c makemove.c perft.c -o vice
+	gcc vice.c init.c bitboards.c hashkeys.c board.c data.c attack.c io.c movegen.c validate.c makemove.c perft.c search.c -o vice
